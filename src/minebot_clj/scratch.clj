f75b9842ddc1a4f301f43e6d10956d07c2c49680
Adrian Smith <smith.adriane@gmail.com>
2014-11-24 00:58:36 -0800

    got scratch example to work with refenvironment.

--- src/minebot_clj/scratch.clj
+++ src/minebot_clj/scratch.clj
@@ -1,78 +1,72 @@
-(ns minecraft-clj.scratch
-)
+(ns minebot-clj.scratch
+  (:require [minebot-clj.cell :refer [ref-environment set-form shake! show-ui set-ref  msg
 
 
-(use 'minebot-clj.cell)
+v-
+v+
+vdist
+box
+draw-calls
+->ScratchBlock
+IDraw
+->ScratchGroup
+scratchgroup
+messages
+                                      scratchblock
+                                      draw-shapes
+                                      size
 
-
-(def scratch-env (atom (minebot_clj.cell.Environment. {} {} {})))
-(defmacro with-scratch
-  ([sym]
-     `(swap! scratch-env set-form (quote ~sym) ~sym))
-  ([sym body]
-     `(swap! scratch-env set-form (quote ~sym) (quote ~body))))
-
-(with-scratch env-vals
-  (fn []
-    (:vals @scratch-env)))
-(swap! scratch-env set-val 'set-val
-       #(swap!
-         scratch-env set-val %1 %2))
-(with-scratch find-child
-  (fn [ref name]
-    (.findChild (.window ref) nil name)))
-(with-scratch msg)
-(with-scratch set-form
-  #(swap! scratch-env set-form %1 %2))
-(with-scratch shake
-  (fn [name]
-    (swap! scratch-env shake name)))
-
-
-(with-scratch show-ui)
-(with-scratch make-ui!
-  (apply show-ui ui))
-(with-scratch normalize-ui)
-
-(with-scratch draw-shapes)
-
-(with-scratch selected-block-id nil)
-(with-scratch scratchblock)
-(with-scratch scratchgroup)
+                                      ]]
+            [net.cgrand.enlive-html :as enlive]
+            [minebot-clj.ui :as ui :refer [connect]]
+            [clojure.core.async :refer [go >! >!! <! <!!  put! chan
+                                        close! timeout
+                                        alts! alts!! thread
+                                        ] :as async])
+  (:use [minebot-clj.zipper :exclude [zseq] :as zip])
+  (:use [minebot-clj.evaluable])
+  (:import com.trolltech.qt.core.QCoreApplication)
+  (:import minebot_clj.cell.ScratchBlock)
+  (:import minebot_clj.cell.ScratchGroup)
+  (:gen-class))
 
 
 
+(def renv (atom (ref-environment)))
+(let [ns *ns*]
+  (defmacro r! [name form]
+    `(do
+       (let [form# (quote ~form)
+             evaluable# (if (evaluable? form#)
+                          form#
+                          (->ClojureEvaluable ~ns form#
+                                              (into {}
+                                                    [~@(for [[k _] &env]
+                                                         [(list 'quote k)
+                                                          k])])))
+             deps# (-> (dependencies evaluable#)
+                       (->> (remove (fn [name#]
+                                      (when-let [var# (ns-resolve ~ns name#)]
+                                        (-> var# meta :reactive? not)))))
+                       set 
+                       (disj ~name))]
+         (swap! renv set-form (quote ~name) evaluable# deps#)
+         (dosync
+          (shake! (deref renv) (quote ~name)))
+         (when (instance? clojure.lang.IDeref ~name)
+           (deref ~name))))))
+(defmacro defr [name form]
+  `(do
+     (let [ref# (ref nil)]
+      (defonce ~(vary-meta name assoc :reactive? true) ref#)
+      (swap! renv set-ref (quote ~name) ref#))
+     (r! ~name ~form)))
 
 
-(with-scratch v-)
-(with-scratch v+)
-(with-scratch vdist)
-(with-scratch box)
 
-(swap! scratch-env set-val 'draw-calls
-       draw-calls)
-(swap! scratch-env set-val 'ScratchBlock minebot_clj.cell.ScratchBlock)
-(swap! scratch-env set-val 'IDraw IDraw)
-(swap! scratch-env set-val 'ScratchGroup minebot_clj.cell.ScratchGroup)
-(swap! scratch-env set-val 'zpeek zpeek)
-(swap! scratch-env set-val 'seqz seqz)
-(swap! scratch-env set-val 'zget zget)
-(swap! scratch-env set-val 'zip zip)
-(swap! scratch-env set-val 'zremove zremove)
-(swap! scratch-env set-val 'zedit zedit)
-(swap! scratch-env set-val 'znth znth)
-(swap! scratch-env set-val 'unzip unzip)
 
-(swap! scratch-env set-val 'zzroot zzroot)
-(swap! scratch-env set-val 'zzremove zzremove)
-(swap! scratch-env set-val 'zzseq zzseq)
-(swap! scratch-env set-val 'zzedit zzedit)
-(swap! scratch-env set-val 'zzget zzget)
-(swap! scratch-env set-val 'zzinsert-left zzinsert-left)
-(swap! scratch-env set-val 'zzinsert-right zzinsert-right)
-
-(with-scratch selected-block nil)
-(with-scratch highlight nil)
+(defr selected-block nil)
+(defr highlight nil)
 
 (defn find-scratch-block [zm block]
   (when zm
@@ -89,14 +83,6 @@
 
        (instance? minebot_clj.cell.ScratchBlock obj)
        (first (keep #(find-scratch-block % block) (seqz (zget zm :subblocks))))))))
-(swap! scratch-env set-val
-       'find-scratch-block find-scratch-block)
-
-(swap! scratch-env set-val
-       'find-scratch-block find-scratch-block)
-
-(swap! scratch-env set-val
-       'size size)
 
 
 
@@ -177,17 +163,7 @@
    obj))
 
 
-(swap!
- scratch-env set-val
- 'find-clicked
- find-clicked)
-
-(swap!
- scratch-env set-val
- 'flatten-draws
- flatten-draws)
-
-(swap! scratch-env set-val 'blocks
+(defr blocks
   (list
    (scratchgroup [50 50] [(scratchblock "alone")])
    (scratchgroup [200 50] [(scratchblock "hi" [(scratchblock "a")
@@ -199,7 +175,7 @@
                                                              [(scratchblock "money")])])])))
 
 
-(with-scratch ui
+(defr ui
   [:scratch2
    [:QVBoxWidget {:minimumSizeHint [400 800]
                   :pos [750 20]}
@@ -207,78 +183,76 @@
                      (clojure.pprint/pprint blocks))}]
     [:QScratchArea {:background-color [200 200 255]
                     :mouseMoved (fn [this pos]
-                                  (try
-                                    (let [draws (flatten-draws blocks nil)
-                                          points (->> draws
-                                                      (filter #(instance? ScratchBlock (:draws %)))
-                                                      (mapcat (fn [m]
-                                                                (let [draws (:draws m)
-                                                                      context (:context m)
-                                                                      pos (->> context
-                                                                                 (map second)
-                                                                                 (apply v+))
-                                                                      [width height] (size draws)]
-                                                                  [pos
-                                                                   (v+ pos [0 height])
-                                                                   (v+ pos [(int (/ width 2))
-                                                                            (int (/ height 2))])]))))
-                                          [rx ry :as closest]
-                                          (first (->> points
-                                                      (sort-by #(vdist pos %))))]
-                                      
-                                      (if (< (vdist pos closest) 75)
-                                        (set-val 'highlight [:drawRect rx ry 10 10])
-                                        (set-val 'highlight nil)))
-                                   (catch Exception e
-                                     (msg e)))
                                   
-                                  (when selected-block
-                                    (set-val 'selected-block
-                                             (assoc selected-block :pos pos))))
+                                  (let [draws (flatten-draws blocks nil)
+                                        points (->> draws
+                                                    (filter #(instance? minebot_clj.cell.ScratchBlock (:draws %)))
+                                                    (mapcat (fn [m]
+                                                              (let [draws (:draws m)
+                                                                    context (:context m)
+                                                                    pos (->> context
+                                                                             (map second)
+                                                                             (apply v+))
+                                                                    [width height] (size draws)]
+                                                                [pos
+                                                                 (v+ pos [0 height])
+                                                                 (v+ pos [(int (/ width 2))
+                                                                          (int (/ height 2))])]))))
+                                        [rx ry :as closest]
+                                        (first (->> points
+                                                    (sort-by #(vdist pos %))))]
+                                    (dosync
+                                     (if (< (vdist pos closest) 75)
+                                       (r! highlight [:drawRect rx ry 10 10])
+                                       (r! highlight nil)))
+                                    (when selected-block
+                                      (r! selected-block
+                                          (assoc selected-block :pos pos)))))
                     :mouseReleased (fn [this pos]
-                                     (set-val 'highlight nil)
-                                     
-                                     (when selected-block
-                                       (set-val 'selected-block nil)
-                                       (let [block-draws (filter #(instance? ScratchBlock (:draws %))
-                                                                 (flatten-draws (zzseq blocks) nil))
-                                             points (->> block-draws
-                                                         (mapcat (fn [m]
-                                                                   (let [draws (:draws m)
-                                                                         context (:context m)
-                                                                         pos (->> context
-                                                                                  (map second)
-                                                                                  (apply v+))
-                                                                         [width height] (size draws)]
-                                                                     [[draws pos :above]
-                                                                      [draws (v+ pos [0 height]) :below]
-                                                                      [draws (v+ pos [(int (/ width 2))
-                                                                                      (int (/ height 2))]) :inside]]))))
-                                             [block point relative]
-                                             (first (->> points
-                                                         (sort-by #(vdist pos (second %)))))]
-                                         (if (< (vdist pos point) 75)
-                                           (try
-                                             (let [insert-op (case relative
-                                                               :above zzinsert-left
-                                                               :below zzinsert-right
-                                                               :inside #(zzedit %1 update-in [:subblocks] conj %2))]
-                                               (set-val 'blocks
-                                                        (-> block
-                                                            (insert-op
-                                                             (first (:blocks selected-block)))
-                                                            zzroot)))
-                                             (catch Exception e
-                                               (msg e)))
-                                           (set-val 'blocks
-                                                    (conj blocks selected-block)))))
+                                     (dosync
+                                      (r! highlight nil)
+                                      
+                                      (when selected-block
+                                        (r! selected-block nil)
+                                        (let [block-draws (filter #(instance? minebot_clj.cell.ScratchBlock (:draws %))
+                                                                  (flatten-draws (zzseq blocks) nil))
+                                              points (->> block-draws
+                                                          (mapcat (fn [m]
+                                                                    (let [draws (:draws m)
+                                                                          context (:context m)
+                                                                          pos (->> context
+                                                                                   (map second)
+                                                                                   (apply v+))
+                                                                          [width height] (size draws)]
+                                                                      [[draws pos :above]
+                                                                       [draws (v+ pos [0 height]) :below]
+                                                                       [draws (v+ pos [(int (/ width 2))
+                                                                                       (int (/ height 2))]) :inside]]))))
+                                              [block point relative]
+                                              (first (->> points
+                                                          (sort-by #(vdist pos (second %)))))]
+                                          (if (< (vdist pos point) 75)
+                                            (try
+                                              (let [insert-op (case relative
+                                                                :above zzinsert-left
+                                                                :below zzinsert-right
+                                                                :inside #(zzedit %1 update-in [:subblocks] conj %2))]
+                                                (r! blocks
+                                                    (-> block
+                                                        (insert-op
+                                                         (first (:blocks selected-block)))
+                                                        zzroot)))
+                                              (catch Exception e
+                                                (msg e)))
+                                            (r! blocks
+                                                (conj blocks selected-block))))))
                                      )
                     :mousePressed (fn [this [x y]]
                                     (try
                                      (let [clicked
                                            (->> (find-clicked [x y]
                                                               (zzseq blocks))
-                                                (filter #(instance? ScratchBlock (first %)))
+                                                (filter #(instance? minebot_clj.cell.ScratchBlock (first %)))
                                                 first
                                                 first)]
                                        
@@ -289,11 +263,11 @@
                                                  selected-block (scratchgroup [x y] [(vary-meta clicked
                                                                                                 dissoc :path)])]
 
-                                             (set-val 'blocks blocks)
-                                             (set-val 'selected-block selected-block)
+                                             (r! blocks blocks)
+                                             (r! selected-block selected-block)
                                              )
                                            (do
-                                             (set-val 'selected-block nil)))
+                                             (r! selected-block nil)))
                                          ))
                                      (catch Exception e
                                            (msg e))))
@@ -308,4 +282,4 @@
 
 
 
-
+(defr ui! (show-ui :scratch3 (second ui)))
