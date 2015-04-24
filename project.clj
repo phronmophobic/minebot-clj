9d20e662a9d9f5178db4538f0177f8fc5cc9f680
Adrian Smith <smith.adriane@gmail.com>
2015-04-24 15:36:05 -0700

    updated to clojure 1.7.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [;;[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0-beta2"]
                  ;; [org.clojure/clojure "1.5.1"]
 
                  [clj-http "0.7.8"]
